0f235775990125387c98332038f6ea5bd0b0c914
Adam Jenkins <adam.jenkins69@me.com>
2026-03-05 18:02:52 +0000

    Add addProduct endpoint for admin page

--- src/main/java/com/dieselscosyscents/waxmeltshop/product/ProductController.java
+++ src/main/java/com/dieselscosyscents/waxmeltshop/product/ProductController.java
@@ -1,9 +1,8 @@
 package com.dieselscosyscents.waxmeltshop.product;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +21,19 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product savedProduct = productRepository.save(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
     @GetMapping("/search")
     public List<Product> searchProducts(@RequestParam("q") String q) {
         return productRepository.searchProductsByName(q);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable long id) {
+        productRepository.deleteById(id);
     }
 }
